4087f51ae2b66d8b59094f571100458ad3238f32
pyaephyowinn <pyaephyowinn@gmail.com>
2023-01-03 22:59:32 +0630

    wip

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import ErrorPage from "./error-page";
+import { loader as contactLoader } from "./routes/contact";
+import Contact from "./routes/contact";
+import EditContact from "./routes/edit";
+import { action as editAction } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "/contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "/contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "/contacts/:contactId/destroy",
+        action: destroyAction,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
